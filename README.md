ce2e809a1bf76f9805ce7fd08a385a3b78a1644c
yojulab <soqqhr@naver.com>
2022-11-03 15:19:16 +0900

    readme

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 |분류| 주제 | 일반(코드) | 함수(코드) | 설명 |
 |---|---|---|---|---|
 |사칙연산| 덧셈 | [java](https://www.acmicpc.net/source/51269674)| [java](https://www.acmicpc.net/source/51270707)| 두 정수를 합함 |
-|사칙연산| 덧셈2 | [java](https://www.acmicpc.net/source/51272172)| [java](https://www.acmicpc.net/problem/10950)| a만큼 반복하는 두 정수를 합함 |
+|사칙연산| 덧셈2 | [java](https://www.acmicpc.net/source/51272172)| [java](https://www.acmicpc.net/source/51273463)| a만큼 반복하는 두 정수를 합함 |
--- src/caseAB.java
+++ src/caseAB.java
@@ -8,7 +8,7 @@ public class caseAB {
     }
     public static void main(String[] args) {
         // 문제 :  첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
-        // 유추파악 : a+b를 t만큼 반복하기 
+        // 유추파악 : a+b를 t만큼 반복 처리 후 출력  
         // 단어 선정 : 입력 input 출력 output 합 plus 반복 수 = int a 합칠 수 int b, int c
 
         //입력
@@ -18,13 +18,15 @@ public class caseAB {
 
         int b;
         int c;
-        //출력
-        caseAB caseAB = new caseAB();
+        
 
         for (int i = 0; i < a; i++) {
             b = sc.nextInt();
             c = sc.nextInt();
-            int output_plus = caseAB.plus(b, c);
+            //처리 
+            caseAB main = new caseAB();           
+            int output_plus = main.plus(b, c);
+            //출력
             System.out.println(output_plus);
         }
 
