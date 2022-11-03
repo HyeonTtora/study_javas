ee7f184e416c195f89f875be391553520d2fc996
yojulab <soqqhr@naver.com>
2022-11-03 15:03:13 +0900

    reademeup

--- src/caseAB.java
+++ src/caseAB.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class caseAB {
+
+    public int plus(int input_first, int input_second) {
+        int output_plus = input_first + input_second;
+        return output_plus;
+    }
     public static void main(String[] args) {
         // 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
         // 입력 : 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
@@ -14,10 +19,13 @@ public class caseAB {
         int b;
         int c;
         //출력
+        caseAB caseAB = new caseAB();
+
         for (int i = 0; i < a; i++) {
             b = sc.nextInt();
             c = sc.nextInt();
-            System.out.println(b + c);
+            int output_plus = caseAB.plus(b, c);
+            System.out.println(output_plus);
         }
 
     }
